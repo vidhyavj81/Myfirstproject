b5955a3ef2af9b2f5ecdc7142374ae4f879f789b
vidhyavj81 <160514668+vidhyavj81@users.noreply.github.com>
2024-02-22 19:27:24 +0530

    My first commit

---
+++ NameAgeS.java
@@ -0,0 +1,15 @@
+class NameAgeS{
+
+public void displayNameAge(String s,int a){
+System.out.println(s);
+System.out.println(a);
+}
+
+public static void main(String args[]){
+
+NameAgeS v=new NameAgeS();
+v.displayNameAge("Vidhya",18);
+
+
+}
+}
