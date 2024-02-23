e1853d0c4bb33a231a4d5ae021fb8db61212cf2e
DELL <DELL@DESKTOP-CTCH6FD>
2024-02-23 19:35:19 +0530

    changed the file.

--- src/com/obsqura/training/method/Test.java
+++ src/com/obsqura/training/method/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// SysteTODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Hello World !!");
 
 	}
 
