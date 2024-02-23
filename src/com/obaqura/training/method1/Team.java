af1cca588cb2414f723609f24542769711db0fcf
DELL <DELL@DESKTOP-CTCH6FD>
2024-02-23 19:27:10 +0530

    My first commit through eclipse.

---
+++ src/com/obaqura/training/method1/Team.java
@@ -0,0 +1,14 @@
+package com.obaqura.training.method1;
+
+public class Team {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=9;
+		int b=8;
+		int sum=a+b;
+		System.out.println("Sum of two numbers "  + sum);
+
+	}
+
+}
---
+++ src/com/obsqura/training/method/Test.java
@@ -0,0 +1,11 @@
+package com.obsqura.training.method;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// SysteTODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
